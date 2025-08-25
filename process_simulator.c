0c71c5d6cdbe4b518bea6c1ea8f2053ae06378bc
DiegoAYF <day7605@mavs.uta.edu>
2025-08-25 17:48:21 -0500

    Enhanced process simulator via web interface

--- process_simulator.c
+++ process_simulator.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
-    printf("Process Simulator v1.0 - Web Version\n");
-    printf("====================================\n");
-    printf("This version was created via GitHub web interface\n");
+    printf("Process Simulator v2.0 - Enhanced Web Version\n");
+    printf("=============================================\n");
+    printf("This enhanced version was created via GitHub web interface\n");
+    printf("Now with additional system headers!\n");
+    
+    printf("Current working directory capabilities added\n");
     return 0;
 }
